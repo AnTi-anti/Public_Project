6d50ee0171b70675c2f876844f3785d7049fa550
AnTi-anti <weilei.gakki@gmail.com>
2022-01-24 09:20:32 +0800

    FastColoredTextBox-master完美支持中文

---
+++ FastColoredTextBox-master/Tester/ReadOnlyBlocksSample.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Tester
+{
+    partial class ReadOnlyBlocksSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fctb = new FastColoredTextBoxNS.FastColoredTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fctb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(491, 58);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "The example shows how to create ReadOnly blocks of the text. \r\nFCTB contains some" +
+    " HTML. The user can to change bodies of tags, but he can not to change <tags>.";
+            // 
+            // fctb
+            // 
+            this.fctb.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fctb.AutoIndentCharsPatterns = "";
+            this.fctb.AutoScrollMinSize = new System.Drawing.Size(410, 105);
+            this.fctb.BackBrush = null;
+            this.fctb.CharHeight = 15;
+            this.fctb.CharWidth = 7;
+            this.fctb.CommentPrefix = null;
+            this.fctb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fctb.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fctb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fctb.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fctb.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.fctb.IsReplaceMode = false;
+            this.fctb.Language = FastColoredTextBoxNS.Language.HTML;
+            this.fctb.LeftBracket = '<';
+            this.fctb.LeftBracket2 = '(';
+            this.fctb.Location = new System.Drawing.Point(0, 58);
+            this.fctb.Name = "fctb";
+            this.fctb.Paddings = new System.Windows.Forms.Padding(0);
+            this.fctb.RightBracket = '>';
+            this.fctb.RightBracket2 = ')';
+            this.fctb.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fctb.Size = new System.Drawing.Size(491, 322);
+            this.fctb.TabIndex = 0;
+            this.fctb.Text = "\r\n<html>\r\n<body>\r\n    <h1> Hello, this text is writable! </h1>\r\n    <div class=re" +
+    "adonly_attribute> Writable text </div>\r\n</body>\r\n</html>";
+            this.fctb.Zoom = 100;
+            this.fctb.KeyPressing += new System.Windows.Forms.KeyPressEventHandler(this.fctb_KeyPressing);
+            // 
+            // ReadOnlyBlocksSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 380);
+            this.Controls.Add(this.fctb);
+            this.Controls.Add(this.label2);
+            this.Name = "ReadOnlyBlocksSample";
+            this.Text = "ReadOnlyBlocksSample";
+            ((System.ComponentModel.ISupportInitialize)(this.fctb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox fctb;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ FastColoredTextBox-master/Tester/Sandbox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using FastColoredTextBoxNS;
+
+namespace Tester
+{
+    public partial class Sandbox : Form
+    {
+        private FastColoredTextBox fctb;
+        TextStyle brownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Regular);
+        TextStyle blueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Bold);
+
+        public Sandbox()
+        {
+            InitializeComponent();
+
+            fctb = new FastColoredTextBox() { Dock = DockStyle.Fill, Parent = this, Language = Language.XML };
+            fctb.Text = @"For example: ""This is a Text between the chr(34)"". 
+ writing forward the for is blue
+";
+
+            fctb.SelectionStyle = new SelectionStyle(Brushes.Red, Brushes.White);
+
+            fctb.KeyPressing += new KeyPressEventHandler(fctb_KeyPressing);
+        }
+
+        void fctb_KeyPressing(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) //Enter Key
+            {
+                e.Handled = true;
+                fctb.InsertText(Environment.NewLine);
+                fctb.DoAutoIndentIfNeed();
+            }
+            else
+            {
+                e.Handled = true;
+                /*
+                if (fctb.GetStyleIndex(_lastDictatedStyle) >= 0)
+                    fctb.Range.ClearStyle(_lastDictatedStyle);*/
+
+                fctb.InsertText(e.KeyChar.ToString(), fctb.SyntaxHighlighter.BlueStyle, true);
+            }
+        }
+    }
+}
